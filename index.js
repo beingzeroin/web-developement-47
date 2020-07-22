c92112a94393438bd31fe506f27a1072ba4a7499
Sandeep <sandy.wadhwa@gmail.com>
2020-07-22 07:36:58 +0530

    Simple Hello World Site

---
+++ index.js
@@ -0,0 +1,19 @@
+// 1.  import or #include liek statement for express
+
+var express = require('express');
+
+// 2. Create Application
+var app = express();
+
+// 3.  Define Functiosn to run when someone accesses our site
+app.get('/', function(req, res){
+    res.send('Hello World');
+})
+
+// 4. Run Application on a port ==> 65xxx (65K ports) Mostly we use beyone 3000
+var port= process.env.PORT  || 3000;
+
+// Once site is up, function() will be called automatically
+app.listen(port, function(){
+    console.log("Site Running on http://localhost:"+port);
+});
